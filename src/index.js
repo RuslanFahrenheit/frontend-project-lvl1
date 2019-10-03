615798300f8ae06728799e186edf21a8f81a48dd
Ruslan Kim <div.doctor@gmail.com>
2019-10-03 18:00:23 +0300

    [Step-4] congratulate user after 3 correct answers, exit after wrong answer

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ export const question = (str) => readlineSync.question(str);
 
 export const isEven = (n) => n % 2 === 0;
 
+export const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
 export const greet = () => {
   console.log('Welcome to the Brain Games!');
   console.log('Answer "yes" if number even otherwise answer "no".');
@@ -13,10 +15,25 @@ export const greet = () => {
   console.log(`Hello, ${userName}!`);
 };
 
-export const askIfNumberIsEven = () => {
-  const randomNumber = Math.round(Math.random() * 10);
-  console.log(`Question: ${randomNumber}`);
-  const answer = question('Your answer: ');
+export const askIfNumberIsEven = (gameLevel = 0) => {
+  if (gameLevel === 3) {
+    console.log('Congratulations, Sam!');
+    return;
+  }
 
-  console.log(isEven(randomNumber));
+  const MIN_VALUE = 1;
+  const MAX_VALUE = 100;
+  const randomNumber = getRandomInt(MIN_VALUE, MAX_VALUE);
+  console.log(`Question: ${randomNumber}`);
+
+  const answerInput = String(question('Your answer: '));
+  const correctAnswer = isEven(randomNumber) ? 'yes' : 'no';
+
+  if (answerInput === correctAnswer) {
+    console.log('Correct!');
+    askIfNumberIsEven(gameLevel + 1);
+  } else {
+    console.log(`'${answerInput}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+    console.log('Let\'s try again, Bill!');
+  }
 };
