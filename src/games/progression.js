e4352132b8d0bbd6cb9d9eceadfc11c060a9c3b0
Ruslan Kim <div.doctor@gmail.com>
2019-10-25 15:40:20 +0300

    [Step-9] refactor(progression.js)
    make progression to be list

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,28 +1,51 @@
+import {
+  cons,
+  head,
+  tail,
+  reverse,
+  isEmpty,
+} from '@hexlet/pairs-data';
 import getRandomInt from '../getRandomInt';
 import { makeGame } from '../api';
 import buildGame from '..';
 
 const progressionSize = 10;
+const progressionStart = getRandomInt(0, 20);
+const progressionDifference = getRandomInt(2, 30);
+const hiddenElementIndex = getRandomInt(0, progressionSize - 1);
 const description = 'What number is missing in the progression?';
 
-export const createGameData = () => {
-  const hiddenElementIndex = getRandomInt(2, progressionSize);
-  const startStep = getRandomInt(2, 20);
-  const progressionStepsDifference = getRandomInt(2, 30);
-
-  const iter = (counter, currentStep) => {
-    if (counter - 1 === progressionSize) {
-      return currentStep;
+const generateProgression = (start, difference, size) => {
+  const iter = (counter, acc) => {
+    if (counter === size - 1) {
+      return reverse(acc);
     }
 
-    const currentProgressionStep = startStep + (counter - 1) * progressionStepsDifference;
-    const nextStep = (counter === hiddenElementIndex) ? '..' : currentProgressionStep;
-
-    return iter(counter + 1, `${currentStep} ${nextStep}`);
+    const currentStep = head(acc) + difference;
+    return iter(counter + 1, cons(currentStep, acc));
   };
 
-  const task = iter(2, startStep);
-  const correctAnswer = startStep + progressionStepsDifference * (hiddenElementIndex - 1);
+  return iter(0, cons(start, null));
+};
+
+export const createGameData = () => {
+  const progression = generateProgression(progressionStart, progressionDifference, progressionSize);
+
+  const iter = (counter, list, acc) => {
+    if (isEmpty(list)) {
+      return acc;
+    }
+
+    if (counter === hiddenElementIndex) {
+      return iter(counter + 1, tail(list), `${acc} ..`);
+    }
+
+    return iter(counter + 1, tail(list), `${acc} ${head(list)}`);
+  };
+
+  const task = iter(0, progression, '');
+
+  const correctAnswer = progressionStart + hiddenElementIndex * progressionDifference;
 
   return makeGame(task, correctAnswer);
 };
